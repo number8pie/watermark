aebed16bff088e2d5c8935df342d1a6eb027ccbd
Lee Thomas <number8pie@gmail.com>
2016-03-29 14:17:51 +0100

    Added form: text to use in watermark, file upload button and submit. Added courier new font file in root. Created function watermarkMe() that takes two parameters, the image to watermark and the text to watermark on the image. Checked if the $_POST variable with the key of submit is set and if so set the image and text variables and pass them to the watermarkMe() function. Script now takes a jpg image and overlays whatever is put in the text box. NEXT: -Make it take png and gif images, and output an error message if it's not one of these files. -Check that the image submitted is a valid image. -Adjust watermark color and transparency. -Add lines in intervals to the watermark, same color and transparrency as watermark text. -Analyse project and notes.

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
+<?php
+
+function watermarkMe($wm_img, $wm_txt){
+  list($width, $height) = getimagesize($wm_img);
+  
+  $image_p = imagecreatetruecolor($width, $height);
+  $image = imagecreatefromjpeg($wm_img);
+  imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width, $height);
+  
+  $black = imagecolorallocate($image_p, 0, 0, 0);
+  $font = 'CourierNewBold.ttf';
+  $fontsize = 50;
+
+  imagettftext($image_p, $fontsize, 0, 10, ($height / 2), $black, $font, $wm_txt);
+  if (isset($wm_img) && isset($wm_txt)) {
+    header('Content-Type: image/jpeg');
+    imagejpeg($image_p, null, 100);
+  }
+
+  imagedestroy($image);
+  imagedestroy($image_p);
+}
+
+if (isset($_POST['submit'])) {
+  $wm_img = $_FILES['wm_img']['tmp_name'];
+  $wm_txt = $_POST['wm_txt'];
+  watermarkMe($wm_img, $wm_txt);
+}
+
+?>
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation | Welcome</title>
+    <title>Watermark Me</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" href="css/app.css" />
   </head>
@@ -12,151 +42,19 @@
 
     <div class="row">
       <div class="large-12 columns">
-        <h1>Add a Watermark to Your Image</h1>
+        <h1>Add a watermark to your image</h1>
       </div>
     </div>
 
     <div class="row">
       <div class="large-12 columns">
-        <div class="callout large">
-          <h3>We&rsquo;re stoked you want to try Foundation! </h3>
-          <p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
-          <p>Once you've exhausted the fun in this document, you should check out:</p>
-          <div class="row">
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://foundation.zurb.com/sites/docs/">Foundation Documentation</a><br />Everything you need to know about using the framework.</p>
-            </div>
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://zurb.com/university/code-skills">Foundation Code Skills</a><br />These online courses offer you a chance to better understand how Foundation works and how you can master it to create awesome projects.</p>
-            </div>
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://foundation.zurb.com/forum">Foundation Forum</a><br />Join the Foundation community to ask a question or show off your knowlege.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 medium-push-2 columns">
-              <p><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</p>
-            </div>
-            <div class="large-4 medium-4 medium-pull-2 columns">
-              <p><a href="https://twitter.com/ZURBfoundation">@zurbfoundation</a><br />Ping us on Twitter if you have questions. When you build something with this we'd love to see it (and send you a totally boss sticker).</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="large-8 medium-8 columns">
-        <h5>Here&rsquo;s your basic grid:</h5>
-        <!-- Grid Example -->
-
-        <div class="row">
-          <div class="large-12 columns">
-            <div class="callout ">
-              <p><strong>This is a twelve column section in a row.</strong> Each of these includes a div.callout element so you can see where the columns are - it's not required at all for the grid.</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="large-6 medium-6 columns">
-            <div class="callout">
-              <p>Six columns</p>
-            </div>
-          </div>
-          <div class="large-6 medium-6 columns">
-            <div class="callout">
-              <p>Six columns</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout">
-              <p>Four columns</p>
-            </div>
-          </div>
-        </div>
-
-        <hr />
-
-        <h5>We bet you&rsquo;ll need a form somewhere:</h5>
-        <form>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-12.columns" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <div class="input-group">
-                <input type="text" placeholder="small-9.columns" class="input-group-field" />
-                <span class="input-group-label">.com</span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Select Box</label>
-              <select>
-                <option value="husker">Husker</option>
-                <option value="starbuck">Starbuck</option>
-                <option value="hotdog">Hot Dog</option>
-                <option value="apollo">Apollo</option>
-              </select>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-6 medium-6 columns">
-              <label>Choose Your Favorite</label>
-              <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Radio 1</label>
-              <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Radio 2</label>
-            </div>
-            <div class="large-6 medium-6 columns">
-              <label>Check these out</label>
-              <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
-              <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Textarea Label</label>
-              <textarea placeholder="small-12.columns"></textarea>
-            </div>
-          </div>
+        <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+          <label for="wm_txt">Text to watermark image with:</label>
+          <input type="text" id="wm_txt" name="wm_txt" value="<?php if(!empty($wm_txt)) { echo $wm_txt; } ?>"></input>
+          <label for="wm_img">Image you want to watermark:</label>
+          <input type="file" id="wm_img" name="wm_img"></input>
+          <input class="button" type="submit" name="submit" value="Watermark Me!"></input>
         </form>
-      </div>
-
-      <div class="large-4 medium-4 columns">
-        <h5>Try one of these buttons:</h5>
-        <p><a href="#" class="small button">Simple Button</a><br/>
-        <a href="#" class="medium success button">Success Button</a><br/>
-        <a href="#" class="medium alert button">Alert Button</a><br/>
-        <a href="#" class="medium secondary button">Secondary Button</a></p>
-        <div class="callout">
-          <h5>So many components, girl!</h5>
-          <p>A whole kitchen sink of goodies comes with Foundation. Check out the docs to see them all, along with details on making them your own.</p>
-          <a href="http://foundation.zurb.com/docs/" class="small button">Go to Foundation Docs</a>
-        </div>
       </div>
     </div>
 
